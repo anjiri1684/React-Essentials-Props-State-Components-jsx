cc86d21fdb6ab660c5ce099a0b42b5b649d86317
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-04 15:02:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
